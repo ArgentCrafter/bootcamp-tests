86ddfd1bc65ac8eab9c37da7255eea582f16d3b4
Jaden <Jaden_Hope@yahoo.com>
2021-04-26 11:43:42 +0200

    updated tests

--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -5,5 +5,8 @@ describe('The mostProfitableDepartment function' , function(){
     it('When given [{"department":"hardware","sales":4500,"day":"Monday"},{"department":"outdoor","sales":1500,"day":"Monday"},{"department":"carpentry","sales":5500,"day":"Monday"},{"department":"hardware","sales":7500,"day":"Tuesday"},{"department":"outdoor","sales":2505,"day":"Tuesday"},{"department":"carpentry","sales":1540,"day":"Tuesday"},{"department":"hardware","sales":1500,"day":"Wednesday"},{"department":"outdoor","sales":8507,"day":"Wednesday"},{"department":"carpentry","sales":8009,"day":"Wednesday"},{"department":"hardware","sales":12000,"day":"Thursday"},{"department":"carpentry","sales":6109,"day":"Thursday"},{"department":"hardware","sales":7005,"day":"Friday"},{"department":"outdoor","sales":12006,"day":"Friday"},{"department":"carpentry","sales":16109,"day":"Friday"}], output should be carpentry' , function(){
         assert.equal(mostProfitableDepartment([{"department":"hardware","sales":4500,"day":"Monday"},{"department":"outdoor","sales":1500,"day":"Monday"},{"department":"carpentry","sales":5500,"day":"Monday"},{"department":"hardware","sales":7500,"day":"Tuesday"},{"department":"outdoor","sales":2505,"day":"Tuesday"},{"department":"carpentry","sales":1540,"day":"Tuesday"},{"department":"hardware","sales":1500,"day":"Wednesday"},{"department":"outdoor","sales":8507,"day":"Wednesday"},{"department":"carpentry","sales":8009,"day":"Wednesday"},{"department":"hardware","sales":12000,"day":"Thursday"},{"department":"carpentry","sales":6109,"day":"Thursday"},{"department":"hardware","sales":7005,"day":"Friday"},{"department":"outdoor","sales":12006,"day":"Friday"},{"department":"carpentry","sales":16109,"day":"Friday"}]), "carpentry");
     });
+    it('When given "jeff", output should be "Please enter a valid list"' , function(){
+        assert.equal(mostProfitableDepartment("jeff"), "Please enter a valid list");
+    });
 
-});
+});s
